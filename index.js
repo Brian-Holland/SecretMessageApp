e5af0d5b8eb4259752190f95783e72c8e400c86d
Brian <brian.holland.pr@gmail.com>
2020-03-26 22:13:17 -0700

    added link url conversion to share

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 document.querySelector("form").addEventListener("submit", event => {
     event.preventDefault();
 
+    //grab messages from user input
     const input = document.querySelector("#message-input");
-
+    //encrpyt message
     const encrypted = btoa(input.value);
-
-    document.querySelector("#link-input").value = encrypted;
+    //select link
+    const linkInput = document.querySelector("#link-input");
+    //set sendable link to url with encrypted message
+    linkInput.value = `${window.location}#${encrypted}`;
+    //auto select for user
+    linkInput.select();
 });
